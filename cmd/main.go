cf6640560d64557a6d72d2231d0185465dc3b781
DESKTOP-RC54URQ\ent1k <ent1k1337@yandex.ru>
2022-12-17 15:57:10 +0300

    added config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"github.com/spf13/viper"
+	"log"
+	todo "todo-app"
+	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
+)
 
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initiazing confogs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	server := new(todo.Server)
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
